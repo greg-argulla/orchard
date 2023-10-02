f84bec0e5f9c2858e34e42f1af78eef24d068787
greg-argulla <greg.argulla@gmail.com>
2023-10-03 00:30:51 +0800

    feat: add dialog to MainDisplay, console log article

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,12 +1,3 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../shadcn/Dialog";
-
 type ArticleType = {
   image: string;
   title: string;
@@ -22,32 +13,25 @@ const Article = (props: ArticleType) => {
   const { image, title, details } = props;
   return (
     <article className="mb-8 w-full px-2 md:w-1/3">
-      <Dialog>
-        <DialogTrigger asChild>
-          <div
-            className="mb-5 h-64 w-full cursor-pointer border-b-4 border-red-600 bg-cover"
-            style={{ backgroundImage: `url(${image}` }}
-          ></div>
-        </DialogTrigger>
-        <DialogContent className="bg-black bg-opacity-70 sm:max-w-4xl">
-          <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogDescription>{details}</DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div
-              className="mb-5 h-[400px] w-full bg-cover"
-              style={{ backgroundImage: `url(${image}` }}
-            ></div>
-          </div>
-        </DialogContent>
-      </Dialog>
-
+      <div
+        className="mb-5 h-64 w-full cursor-pointer border-b-4 border-red-600 bg-cover"
+        style={{ backgroundImage: `url(${image}` }}
+        onClick={() => {
+          console.log(`Clicked ${title}`);
+        }}
+      ></div>
       <div className="mb-1 font-bold">{title}</div>
       <div className="gray mb-4 font-light text-white text-opacity-70">
         {details}
       </div>
-      <span className="border-b-2 border-red-700 p-1 font-bold">READ MORE</span>
+      <a
+        className="cursor-pointer border-b-2 border-red-700 p-1 font-bold"
+        onClick={() => {
+          console.log(`Clicked ${title}`);
+        }}
+      >
+        READ MORE
+      </a>
     </article>
   );
 };
@@ -60,8 +44,8 @@ const Articles = (props: ArticlesProps) => {
         {header}
       </header>
       <div className="flex flex-col md:flex-row">
-        {articles.map((props) => (
-          <Article {...props} />
+        {articles.map((props, index) => (
+          <Article key={index} {...props} />
         ))}
       </div>
     </section>
--- src/components/MainDisplay/index.tsx
+++ src/components/MainDisplay/index.tsx
@@ -1,3 +1,13 @@
+import { prependOnceListener } from "process";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../shadcn/Dialog";
+import { ReactNode } from "react";
 export interface MainDisplayProps {
   image1: string;
   image2: string;
@@ -8,24 +18,44 @@ export interface MainDisplayProps {
   warning: string;
 }
 
+type ModalProps = {
+  children: ReactNode;
+  image: string;
+};
+
+const ModalImage = (props: ModalProps) => (
+  <Dialog>
+    <DialogTrigger asChild>{props.children}</DialogTrigger>
+    <DialogContent className=" max-w-3xl bg-black bg-opacity-70">
+      <img src={props.image} alt={props.image}></img>
+    </DialogContent>
+  </Dialog>
+);
+
 const MainDisplay = (props: MainDisplayProps) => {
   const { image1, image2, image3, header, content, footer, warning } = props;
   return (
     <section className="relative mb-32 mt-32 flex min-h-[540px] min-w-[320px] flex-wrap px-5 sm:flex-nowrap sm:gap-5 lg:px-0">
-      <div
-        className="h-[400px] w-3/6 bg-cover bg-center sm:mr-0 sm:h-[540px] sm:w-3/6 lg:w-2/6"
-        style={{ backgroundImage: `url(${image1}` }}
-      ></div>
+      <ModalImage image={image1}>
+        <div
+          className="h-[400px] w-3/6 bg-cover bg-center sm:mr-0 sm:h-[540px] sm:w-3/6 lg:w-2/6"
+          style={{ backgroundImage: `url(${image1}` }}
+        ></div>
+      </ModalImage>
 
       <div className="flex h-[400px] w-3/6 flex-col sm:h-[540px] sm:w-3/6 sm:gap-5 lg:w-2/6">
-        <div
-          className="h-full transform bg-cover bg-center"
-          style={{ backgroundImage: `url(${image2}` }}
-        ></div>
-        <div
-          className="h-full transform bg-cover bg-center"
-          style={{ backgroundImage: `url(${image3}` }}
-        ></div>
+        <ModalImage image={image2}>
+          <div
+            className="h-full transform bg-cover bg-center"
+            style={{ backgroundImage: `url(${image2}` }}
+          ></div>
+        </ModalImage>
+        <ModalImage image={image3}>
+          <div
+            className="h-full transform bg-cover bg-center"
+            style={{ backgroundImage: `url(${image3}` }}
+          ></div>
+        </ModalImage>
       </div>
 
       <div className="sw-full top-1/2 min-w-[320px] py-5 sm:absolute sm:-translate-y-1/2 sm:bg-black sm:bg-opacity-60 sm:px-5 lg:relative lg:top-0 lg:w-2/6 lg:translate-y-0 lg:bg-transparent lg:p-0">
