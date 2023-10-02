ee86a334ed33d3647758c30447acf8abe6690bf8
greg-argulla <greg.argulla@gmail.com>
2023-10-03 00:03:45 +0800

    fix: add shadcn dialog

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../shadcn/Dialog";
+
 type ArticleType = {
   image: string;
   title: string;
@@ -13,10 +22,27 @@ const Article = (props: ArticleType) => {
   const { image, title, details } = props;
   return (
     <article className="mb-8 w-full px-2 md:w-1/3">
-      <div
-        className="mb-5 h-64 w-full border-b-4 border-red-600 bg-cover"
-        style={{ backgroundImage: `url(${image}` }}
-      ></div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div
+            className="mb-5 h-64 w-full cursor-pointer border-b-4 border-red-600 bg-cover"
+            style={{ backgroundImage: `url(${image}` }}
+          ></div>
+        </DialogTrigger>
+        <DialogContent className="bg-black bg-opacity-70 sm:max-w-4xl">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{details}</DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div
+              className="mb-5 h-[400px] w-full bg-cover"
+              style={{ backgroundImage: `url(${image}` }}
+            ></div>
+          </div>
+        </DialogContent>
+      </Dialog>
+
       <div className="mb-1 font-bold">{title}</div>
       <div className="gray mb-4 font-light text-white text-opacity-70">
         {details}
