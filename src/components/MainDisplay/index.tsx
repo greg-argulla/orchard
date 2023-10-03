d552296edd7d22238ff11c2d92de9781d0a00f3e
greg-argulla <greg.argulla@gmail.com>
2023-10-03 18:52:44 +0800

    fix: transfer padding to parent for MainDisplay lg

--- src/components/MainDisplay/index.tsx
+++ src/components/MainDisplay/index.tsx
@@ -32,7 +32,7 @@ const ModalImage = (props: ModalProps) => (
 const MainDisplay = (props: MainDisplayProps) => {
   const { image1, image2, image3, header, content, footer, warning } = props;
   return (
-    <section className="relative mb-32 mt-32 flex min-h-[540px] min-w-[320px] flex-wrap px-5 sm:flex-nowrap sm:gap-5 lg:px-0">
+    <section className="relative mb-32 mt-32 flex min-h-[540px] min-w-[320px] flex-wrap px-5 sm:flex-nowrap sm:gap-5 lg:px-5">
       <ModalImage image={image1}>
         <div
           className="h-[400px] w-3/6 bg-cover bg-center sm:mr-0 sm:h-[540px] sm:w-3/6 lg:w-2/6 "
@@ -55,7 +55,7 @@ const MainDisplay = (props: MainDisplayProps) => {
         </ModalImage>
       </div>
 
-      <div className="sw-full top-1/2 min-w-[320px] py-5 sm:absolute sm:mr-5 sm:-translate-y-1/2 sm:bg-black sm:bg-opacity-60 sm:px-5 lg:relative lg:top-0 lg:w-2/6 lg:translate-y-0 lg:bg-transparent lg:p-0 lg:px-5">
+      <div className="sw-full top-1/2 min-w-[320px] py-5 sm:absolute sm:mr-5 sm:-translate-y-1/2 sm:bg-black sm:bg-opacity-60 sm:px-5 lg:relative lg:top-0 lg:w-2/6 lg:translate-y-0 lg:bg-transparent lg:p-0 ">
         <header className="mb-6 border-b-2 pb-2 text-[26px] font-light">
           {header}
         </header>
